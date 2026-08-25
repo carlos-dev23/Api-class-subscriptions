c1198fa45397413b49c988d075a38c83469db5d9
carlos-dev23 <jcsegurat@gmail.com>
2026-08-24 21:30:01 -0600

    feat(database): create migration for lesson table and suscription table

---
+++ database/migrations/2026_08_12_042025_create_lesson_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('maximum_capacity');
+            $table->string('teacher');
+            $table->date('start_lesson');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
